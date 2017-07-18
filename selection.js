6dd2f0b0dfb22948c53877be6ffd95bbf6d4b467
Zhekoay <koayzhe@gmail.com>
2017-07-19 00:51:26 +0800

    Create selection.js

---
+++ selection.js
@@ -0,0 +1,11 @@
+function click(e) {
+  chrome.tabs.executeScript({file: 'content.js'});
+  window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
+});
